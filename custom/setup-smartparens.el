32f7b5d486e17893a2d91cbf6acd9be5e405562c
AaronAn <75335137@qq.com>
2016-10-24 19:10:02 +0800

    smartparens improvement

--- custom/setup-smartparens.el
+++ custom/setup-smartparens.el
@@ -1,7 +1,14 @@
 (use-package smartparens
   :init
   (require 'smartparens-config)
-  (smartparens-global-mode))
+  (show-smartparens-global-mode +1)
+  (smartparens-global-mode 1)
+  ;; when you press RET, the curly braces automatically
+  ;; add another newline
+  (sp-with-modes '(c-mode c++-mode)
+    (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET")))
+    (sp-local-pair "/*" "*/" :post-handlers '((" | " "SPC")
+                                              ("* ||\n[i]" "RET")))))
 
 ;; (define-advice show-paren-function (:around (fn) fix-show-paren-function)
 ;;   "Highlight enclosing parens."
