4b3dddd9ab63d54646067f94d28c990814efc856
AaronAn <75335137@qq.com>
2016-10-29 18:36:38 +0800

    add dev staff list

--- custom/setup-org.el
+++ custom/setup-org.el
@@ -18,7 +18,7 @@
 (setq org-src-fontify-natively t)
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
-(setq org-log-done t)
+;(setq org-log-done t)
 
 (add-hook 'org-mode-hook
           (lambda () (setq truncate-lines nil)))
